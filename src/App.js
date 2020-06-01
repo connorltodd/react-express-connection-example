d59100890406b5b05ad9a7bbb5dcb4654e904ccf
DEV <connortodd0304@gmail.com>
2020-06-01 16:33:12 +0100

    frontend to backend integration implemented

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    users: [],
+    name: '',
+    email: '',
+    password: ''
+  }
+
+  componentDidMount () {
+      axios.get('http://localhost:3000/api/users')
+      .then(response => this.setState({ users: response.data }));
+  }
+
+  handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    this.setState({ [name]: value })
+  }
+
+  addUser = (event) => {
+    event.preventDefault();
+    const { name, email, password } = this.state;
+    axios.post('http://localhost:3000/api/users', {
+      name: name,
+      email: email,
+      password: password
+    })
+    .then(response =>  this.setState({ users: [...this.state.users, response.data]}))
+
+    this.setState({
+      name: '',
+      email: '',
+      password: ''
+    })
+  }
+
+  render () {
+    const { users, name, email, password } = this.state;
+    return (
+      <div className="App">
+        {users.map(user => (
+          <div>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        ))}
+
+        <form onSubmit={this.addUser}>
+          <div>
+            <p>Name</p>
+            <input name="name" value={name} onChange={this.handleChange} />
+          </div>
+          <div>
+            <p>Email</p>
+            <input name="email" value={email} onChange={this.handleChange} />
+          </div>
+          <div>
+            <p>Password</p>
+            <input type="password" name="password" value={password} onChange={this.handleChange} />
+          </div>
+          <button type="submit" >Submit new user</button>
+        </form>
+        <br />
+        <br />
+      </div>
+    );
+  }
 }
 
 export default App;
